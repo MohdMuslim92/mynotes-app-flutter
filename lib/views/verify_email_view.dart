14706c03b33304e13e3c9dbded16ba625a42d486
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-09-24 03:47:29 +0300

    Feat: Enhance verify email view with additional actions
    - Updated the UI to inform users about the verification email and instructions.
    - Included a "Restart" button to log out the user and navigate them back to the registration page.

--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({super.key});
@@ -17,14 +18,27 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
       ),
       body: Column(
         children: [
-          const Text('Please verify your email address'),
+          const Text(
+              'We have sent you an email verification. Please open it to verify your account.'),
+          const Text(
+              'If you have not received a verification email yet, press the button below.'),
           TextButton(
             onPressed: () async {
               final user = FirebaseAuth.instance.currentUser;
               await user?.sendEmailVerification();
             },
             child: const Text('Send email verification'),
-          )
+          ),
+          TextButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                registerRoute,
+                (_) => false,
+              );
+            },
+            child: const Text('Restart'),
+          ),
         ],
       ),
     );
