ac3fc5e698c0171c9c8841c868c4e02819549883
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-09-27 22:28:29 +0300

    Feat: add custom exception classes for CRUD operations
    - Introduced specific exception classes to handle errors in database operations.
    - Added exceptions for scenarios like:
      - Database already open or not open.
      - Unable to access documents directory.
      - CRUD operation failures for users and notes, including finding, deleting, and updating data.
      - User mismatch during note creation.

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,19 @@
+class DatabaseAlreadyOpenException implements Exception {}
+
+class UnableToGetDocumentsDirectoryException implements Exception {}
+
+class DatabaseIsNotOpenExeption implements Exception {}
+
+class CouldNotDeleteUserException implements Exception {}
+
+class UserAlreadyExistsException implements Exception {}
+
+class CouldNotFindUserException implements Exception {}
+
+class DatabaseUserMismatchException implements Exception {}
+
+class CouldNotDeleteNoteException implements Exception {}
+
+class CouldNotFindNoteException implements Exception {}
+
+class CouldNotUpdateNoteException implements Exception {}
