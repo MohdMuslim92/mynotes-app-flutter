0c69f008ad47a8002f7ae825d54973b55014ea18
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-09-29 23:53:15 +0300

    Add logout confirmation dialog
    - Added `showLogOutDialog` using the generic dialog utility to confirm user logout actions.
    - Provides 'Cancel' and 'Log out' options for users, returning true or false based on the selection.
    - Refactors logout dialog creation to ensure consistency with other dialogs in the app.

---
+++ lib/utilities/dialogs/logout_dialog.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:mynotes/utilities/dialogs/generic_dialog.dart';
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showGenericDialog<bool>(
+    context: context,
+    title: 'Logout',
+    content: 'Are you sure you want to logout?',
+    optionsBuilder: () => {
+      'Cancel': false,
+      'Log out': true,
+    },
+  ).then(
+    (value) => value ?? false,
+  );
+}
