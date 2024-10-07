717e780d46123bf0e070175b90569aeea34690bb
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-10-07 04:54:07 +0300

    Feat: implement password reset functionality in AuthBloc
    - Added handling for the `AuthEventForgotPassword` event to manage the password reset process.
    - Introduced the initial state of `AuthStateForgotPassword` to indicate whether a password reset email has been sent or if an error occurred.
    - Emitted loading states while the email is being sent to enhance user feedback.
    - Implemented logic to call `sendPasswordReset` from the AuthProvider and handle potential exceptions.
    - This update improves the user experience by providing clear feedback during the password reset process.

--- lib/services/auth/bloc/auth_bloc.dart
+++ lib/services/auth/bloc/auth_bloc.dart
@@ -6,6 +6,47 @@ import 'package:mynotes/services/auth/bloc/auth_state.dart';
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc(AuthProvider provider)
       : super(const AuthStateUninitialized(isLoading: true)) {
+    on<AuthEventShouldRegister>((event, emit) {
+      emit(const AuthStateRegistering(
+        exception: null,
+        isLoading: false,
+      ));
+    });
+    // Forgot password
+    on<AuthEventForgotPassword>((event, emit) async {
+      emit(const AuthStateForgotPassword(
+        exception: null,
+        isLoading: false,
+        hasSentEmail: false,
+      ));
+      final email = event.email;
+      if (email == null) {
+        return; // User just wants to go to forgot-password screen
+      }
+
+      // User wants an email to be sent
+      emit(const AuthStateForgotPassword(
+        exception: null,
+        isLoading: true,
+        hasSentEmail: false,
+      ));
+
+      bool didSendEmail;
+      Exception? exception;
+      try {
+        await provider.sendPasswordReset(toEmail: email);
+        didSendEmail = true;
+        exception = null;
+      } on Exception catch (e) {
+        didSendEmail = false;
+        exception = e;
+      }
+      emit(AuthStateForgotPassword(
+        exception: exception,
+        isLoading: false,
+        hasSentEmail: didSendEmail,
+      ));
+    });
     // Send verification email
     on<AuthEventSendEmailVerification>((event, emit) async {
       await provider.sendEmailVerification();
